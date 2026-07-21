30626b320001843b2c926603fc894a81381021b6
Awais Ahmed <waisigondal4486@gmail.com>
2026-07-21 17:55:11 +0500

    first commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,160 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState, } from 'react';
+import type { PropsWithChildren } from 'react';
+import { Platform, StyleSheet, View, Text, TouchableOpacity, ImageBackground, Button, Alert, TextInput, Image } from 'react-native';
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+const FlexDirectionBasics = () => {
+  const [flexDirection, setflexDirection] = useState('column');
+
+  const [Username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
-
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={require('./bg.jpg')}
+        style={{ flex: 1 }}
+      >
+        <View
+          style={{
+            alignItems: 'center',
+            marginTop: '20%',
+            // backgroundColor: '#589341',
+          }}
+        >
+          <Image
+            source={require('./logo.jpg')}
+            style={{ width: 120, height: 120, marginTop: '20%', borderRadius: 60, borderWidth: 2, borderColor: '#589341', }}
           />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+        </View>
+        <View style={[{ marginTop: '10%', padding: 20, }]}>
+          <View style={{ backgroundColor: '#589341', padding: 15, borderRadius: 9, marginBottom: 10 }}>
+            <TextInput
+              placeholder="Enter your Username"
+              value={Username}
+              onChangeText={setUsername}
+            />
+          </View>
+          <View style={{ backgroundColor: '#589341', padding: 15, borderRadius: 9, marginBottom: 10 }}>
+            <TextInput
+              placeholder="Enter your Password"
+              secureTextEntry={true}
+              value={password}
+              onChangeText={setPassword}
+            />
+          </View>
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+          <View
+            style={{
+              flexDirection: 'row',
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                padding: 10,
+                borderRadius: 9,
+                backgroundColor: '#589341',
+                // marginBottom: 10,
+                justifyContent: 'space-between',
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  // padding: 10,
+                  // borderRadius: 4,
+                  backgroundColor: '#589341',
+                }}
+                onPress={() => {
+                  Alert.alert(`You Signed In with the Username : ${Username}!`);
+                }}
+              >
+                <Text
+                  style={{ color: 'white', fontSize: 20, textAlign: 'center', padding: 10 }}
+                >
+                  Sign In</Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                marginLeft: 10,
+                padding: 10,
+                borderRadius: 9,
+                backgroundColor: '#589341',
+                // marginBottom: 10,
+                justifyContent: 'space-between',
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  // padding: 10,
+                  // borderRadius: 4,
+                  backgroundColor: '#589341',
+                }}
+                onPress={() => {
+                  Alert.alert(`You Signed In with the Username : ${Username}!`);
+                }}
+              >
+                <Text
+                  style={{ color: 'white', fontSize: 20, textAlign: 'center', padding: 10 }}
+                >
+                  forgot</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+        </View>
+      </ImageBackground>
+    </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
+    paddingLeft: 30,
     flex: 1,
-    justifyContent: 'center',
+    marginTop: 8,
+    backgroundColor: 'aliceblue',
+  },
+  box: {
+    width: 50,
+    height: 50,
+    paddingLeft: 10,
+  },
+  row: {
     flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: '48%',
     textAlign: 'center',
   },
-  code: {
-    textTransform: 'uppercase',
+  selected: {
+    backgroundColor: 'coral',
+    borderWidth: 0,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'coral',
+  },
+  selectedLabel: {
+    color: 'white',
+  },
+  label: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
+
+export default FlexDirectionBasics;
